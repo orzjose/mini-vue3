a5bb5e77ed943875ea74fe1436ad69723b3baec5
orzjose <hi.baomihua@gmail.com>
2022-04-08 10:03:48 +0800

    feat: 实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,7 +1,14 @@
 import { h } from '../../lib/mini-vue3.esm.js'
 export const App = {
   render() {
-    return h('div', {}, 'hello')
+    console.log(this, 'this');
+    return h(
+      'div',
+      {
+        class: [1, 2, 3, 4, 5]
+      },
+      [h('p', { class: 'red' }, '1234'), h('p', { class: 'blue' }, '4567')]
+    )
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from '../shared'
 import { createComponentInstance, setupComponent } from './component'
 
 export function render(vnode, container) {
@@ -5,10 +6,15 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  console.log(vnode, container)
-  // 处理 component
-  processComponent(vnode, container)
+  if (isObject(vnode.type)) {
+    // 处理 component
+    processComponent(vnode, container)
+  } else if (typeof vnode.type === 'string') {
+    // 处理 element
+    processElement(vnode, container)
+  }
 }
+
 function processComponent(vnode, container) {
   mountComponent(vnode, container)
 }
@@ -24,4 +30,25 @@ function setUpRenderEffect(instance, container) {
   const subTree = instance.render()
 
   patch(subTree, container)
+}
+function processElement(vnode, container) {
+  const el: HTMLElement = document.createElement(vnode.type)
+  // attrbuite
+  const { props, children } = vnode
+  for (const key in props) {
+    // vnode.props
+    el.setAttribute(key, props[key])
+  }
+
+  if (typeof children === 'string') {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    renderChildren(children, el)
+  }
+  container.append(el)
+}
+function renderChildren(children, container) {
+  children.forEach((vnode) => {
+    patch(vnode, container)
+  })
 }
